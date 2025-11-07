ea770ddd786d13487c96eb51cb6826f3dbec31db
SkySaksham <skysaksham1608@gmail.com>
2025-11-08 03:25:48 +0530

    Added show:database command in parser

--- query.py
+++ query.py
@@ -1,23 +1,47 @@
 # Attempts to make a query Language
 
+# commands so far
 
 # current commands
 # checkout:<database_name>
 # checkout 
 # drop:database:<database_name>
 # drop:<database_name>
+# show:database
 
+
+import os
 import main as mp
 import create_db as cd
 
 
-command = ["checkout","get","create","drop"]
+command = ["checkout","get","create","drop","show"]
 command_2nd = ["database"]
 
 
 
 database = None
 space = "        "
+
+def get_database() :
+    folders = [["",f,""] for f in os.listdir() if os.path.isdir(f) and f not in ["__pycache__",".git"]]
+    width = 8
+    for i in range (len(folders)) :
+        folders[i][0] = str(i+1)
+        if len(folders[i][1]) > width : width = len(folders[i][1]) 
+        if folders[i][1] == database : folders[i][2] = "TRUE" 
+    
+    width_list = [4,width,8]
+
+    #print (width_list)
+    
+    table = [["S.no","Database","Selected"]] + folders
+
+    mp.draw_table(table,width_list)
+
+
+    
+
 
 def initiate(name) :
     global database
@@ -53,7 +77,8 @@ def parser(query_string) :
         if query[1] == command_2nd[0] :
             try : cd.drop_db(query[2],space)
             except Exception as e : print(e)
-
+    if query [0] == command [4] :
+        if query[1] == command_2nd[0] : get_database()
 
 
 while True :
