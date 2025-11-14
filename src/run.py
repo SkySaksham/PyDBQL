fda1ddf84b35e9b6a61800f5c796e0e831d40d4f
SkySaksham <skysaksham1608@gmail.com>
2025-11-14 23:06:15 +0530

    hihih

--- src/run.py
+++ src/run.py
@@ -3,20 +3,21 @@ import drop_and_create_db as db
 import create_table as ct
 
 
+'''
 
-#db.create_db("hello2")
+#db.create_db("hello")
+
 table = "user"
-a="sno:(key)int(4):Name:()str(12):balance:float"
-entry = ["3:user:90","4:user:1221"]
+a="UID:()int:Name:str:blb:float(10)"
+entry = ["10:user:90",]
 
 try : ad.add_entries("hello",table,entry)
 except Exception as e : print (e)
 
 
-
-
-
-
-#data=ad.table_info(a)
+data=ad.table_info(a)
 #db.create_db("hello")
-#ct.create_table("hello",table,data)
+ct.create_table("hello",table,data)
+
+
+'''
